46d639822ea8863adc2958d37683ef3448e8bf06
David Wuibaille <david.wuibaille@outlook.fr>
2025-03-19 10:49:51 +0100

    Update Gui.ps1

--- BIOSHP/Gui.ps1
+++ BIOSHP/Gui.ps1
@@ -11,8 +11,8 @@ $biosSettings = @{}
 foreach ($Conf in $BiosInfo) {
     $Param = $Conf.Name
     $Valeur = $Conf.Value -join ", "  # Convertit en texte lisible
-    $ActiveValue = ($Conf.Value | Where-Object { $_ -match "\*" }) -join ", "  # Filtrer les valeurs contenant "*"
-    $ActiveValue = $ActiveValue -replace "\*", ""  # Supprimer les "*"
+$ActiveValue = ($Conf.Value -split ", " | Where-Object {$_ -match "\*"}) -replace "\*", ""
+
 
     $ActiveValue = $ActiveValue -replace "\*", ""  # Supprime les "*"
 
